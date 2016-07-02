5c1fb2afca6245055bfab4fc7bde138be8616a45
Aphichat Panjamanee <panjamanee@gmail.com>
2016-07-02 22:52:17 +0200

    Path bugfixes

--- setup.sh
+++ setup.sh
@@ -7,6 +7,6 @@ APACHE_PATH="/etc/apache2/sites-available"
 if [ -f ${APACHE_PATH}/001-cloud9.conf ]; then
   curl ${CONF_FILE} > 001-cloud9.conf
   sudo cp 001-cloud9.conf ${APACHE_PATH}/001-cloud9.conf && rm 001-cloud9.conf
-  mkdir site && cd site && mkdir public
+  mkdir site && mkdir site/public
   curl ${INDEX_FILE} > site/public/index.html
 fi
