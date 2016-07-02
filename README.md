c032395dbd55bfdeca385a3c135c713a762a1bd2
Aphichat Panjamanee <panjamanee@gmail.com>
2016-07-02 23:20:58 +0200

    New setup method

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Initialize PHP, Apache, MySQL workspace for Cloud9
 
 This script rewrite 001-cloud9.conf to change the public html to /site/public and moves the root folder to the site folder
 
-	$ curl https://raw.githubusercontent.com/aphichat/c9-init/master/setup.sh > setup.sh
-	$ sh setup.sh
+	$ git clone git@github.com:aphichat/c9-init
+	$ cd c9-init && sh setup.sh
--- setup.sh
+++ setup.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-CONF_FILE="https://raw.githubusercontent.com/aphichat/c9-init/master/001-cloud9.conf"
-INDEX_FILE="https://raw.githubusercontent.com/aphichat/c9-init/master/index.html"
 APACHE_PATH="/etc/apache2/sites-available"
 
 if [ -f ${APACHE_PATH}/001-cloud9.conf ]; then
-  curl ${CONF_FILE} > 001-cloud9.conf
-  sudo cp 001-cloud9.conf ${APACHE_PATH}/001-cloud9.conf && rm 001-cloud9.conf
-  if [ ! -d site ]; then
-    mkdir site && mkdir site/public
-    curl ${INDEX_FILE} > site/public/index.html
+  sudo cp 001-cloud9.conf ${APACHE_PATH}/001-cloud9.conf
+  if [ ! -d ~/workspace/site ]; then
+    mkdir ~/workspace/site && mkdir ~/workspace/site/public
+    cp index.html > ~/workspace/site/public/index.html
   fi
 fi
