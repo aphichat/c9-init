db4dd5293754a101c3bd520aab2a8f76c87b6936
Aphichat Panjamanee <panjamanee@gmail.com>
2016-07-03 00:12:40 +0200

    Update

--- cms/craft.sh
+++ cms/craft.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-TD_SITE="~/workspace/site"
+TD_REPO="git@github.com:28desk/craft"
 
-if [ ! "$(ls -A ${TD_SITE}/public)" ]; then
-  rm -r ${TD_SITE}/public
-  cd $TD_PUBLIC && git clone git@github.com:28desk/craft .
-  mkdir ${TD_SITE}/public
-  echo "Success! Craft initialized"
-else
+if [ "$(ls -A ~/workspace/site/public)" ]; then
   echo "Error! public folder is not empty"
+else
+  mkdir ~/workspace/site/public && git clone $TD_REPO ~/workspace/site/public
+  echo "Success! Craft initialized"
 fi
--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 TD_CONF_FILE="etc/apache2/sites-available/001-cloud9.conf"
 
 sudo cp ${TD_CONF_FILE} /${TD_CONF_FILE}
-if [ ! -d ~/workspace/site ]; then
+if [ -d ~/workspace/site ]; then
+  echo "Error! site folder is not empty"
+else
   mkdir ~/workspace/site
   echo "Success! Created public folder for server root"
-else
-  echo "Error! site folder is not empty"
 fi
