9c73f362a44f8c9cdde0f9aa1779f50af6989aaa
Aphichat Panjamanee <panjamanee@gmail.com>
2016-07-02 23:23:26 +0200

    Fixed when cp and the index.html content gets removed

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ if [ -f ${APACHE_PATH}/001-cloud9.conf ]; then
   sudo cp 001-cloud9.conf ${APACHE_PATH}/001-cloud9.conf
   if [ ! -d ~/workspace/site ]; then
     mkdir ~/workspace/site && mkdir ~/workspace/site/public
-    cp index.html > ~/workspace/site/public/index.html
+    cp index.html ~/workspace/site/public/index.html
   fi
 fi
